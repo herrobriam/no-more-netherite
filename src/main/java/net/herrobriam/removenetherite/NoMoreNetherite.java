37fae62d6c021b158fab2ff7181e19b8b3415d25
herrobriam <131239470+herrobriam@users.noreply.github.com>
2024-07-27 17:24:46 +0100

    Initial commit

---
+++ src/main/java/net/herrobriam/removenetherite/NoMoreNetherite.java
@@ -0,0 +1,13 @@
+package net.herrobriam.removenetherite;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoMoreNetherite implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("no-more-netherite");
+
+	@Override
+	public void onInitialize() {
+	}
+}
